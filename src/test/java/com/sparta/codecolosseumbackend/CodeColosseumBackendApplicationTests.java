847986308e8fa4c6da2ed7b7393fc99fa57ce18b
yuns8708 <yuns8708@gmail.com>
2022-10-21 15:09:33 +0900

    프로젝트 시작

---
+++ src/test/java/com/sparta/codecolosseumbackend/CodeColosseumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.codecolosseumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeColosseumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
