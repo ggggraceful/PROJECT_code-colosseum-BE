a9982260fb354e84feae024f25911232dd8e369e
soojin-dev <dvxz1125@gmail.com>
2022-10-26 11:22:20 +0900

    [Update] 아이디 양식 변경

--- src/main/java/com/sparta/codecolosseumbackend/controller/MemberController.java
+++ src/main/java/com/sparta/codecolosseumbackend/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -32,4 +33,9 @@ public class MemberController {
         return memberService.login(loginRequestDto, httpServletResponse);
     }
     //로그아웃
+//    @PostMapping("/api/logout")
+//    public void logout(HttpServletRequest request, HttpServletResponse response, LogOutUrl logOutUrl) {
+//        System.out.println("로그아웃 성공");
+//        userService.logout(request, response, logOutUrl);
+//    }
 }
